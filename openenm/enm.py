2928481f5f95e9d411f6ce184d05710ce4073c0a
Diego Prada-Gracia <prada.gracia@gmail.com>
2022-07-29 23:17:00 -0500

    Some bugs fixed in the ENM class

--- openenm/enm.py
+++ openenm/enm.py
@@ -2,23 +2,27 @@ import molsysmt as msm
 
 class ENM():
 
-    def __init__(self, molecular_system, selection='atom_name=="CA"', cutoff='12 angstroms',
+    def __init__(self, molecular_system, structure_index=0, selection='atom_name=="CA"', cutoff='12 angstroms',
                  syntax='MolSysMT'):
 
         self.molecular_system = molecular_system
         self.selection = selection
         self.syntax = syntax
         self.cutoff = cutoff
+        self.structure_index = structure_index
 
-        self.contact_map = None
+        self.contacts = None
 
         # Start getting the contact map
 
-        self.initialize_contact_map()
+        self.initialize_contacts()
 
-    def initialize_contact_map(self):
+    def initialize_contacts(self):
 
-        self.contact_map = msm.structure.get_contacts(self.molecular_system,
-                                                      selection=self.selection,
-                                                      threshold=self.cutoff, syntaxis=self.syntax)
+        contacts = msm.structure.get_contacts(self.molecular_system,
+                                              selection=self.selection,
+                                              structure_indices=self.structure_index,
+                                              threshold=self.cutoff, syntaxis=self.syntax)
+
+        self.contacts = contacts[0]
 
